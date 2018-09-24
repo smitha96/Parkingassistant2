ea5b4fe213bb3bd391a384e759f45332baea7e13
smitha96 <smithamurthy96@gmail.com>
2018-09-24 13:16:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/smitha/parkingassistant/Firstscreen.java
@@ -0,0 +1,40 @@
+package com.example.smitha.parkingassistant;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Firstscreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN_OUT=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firstscreen);
+        hideNavBar();
+        ImageView imageView=findViewById(R.id.carimage);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent=new Intent(Firstscreen.this,UserLogin.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_SCREEN_OUT);
+    }
+    private void hideNavBar(){
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
+}
