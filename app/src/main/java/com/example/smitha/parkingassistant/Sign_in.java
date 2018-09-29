9d9dc3deb6acd85067fc98a6b9d1b41d132a1cf6
smitha96 <smithamurthy96@gmail.com>
2018-09-29 19:38:33 +0530

    Signup and login done

--- app/src/main/java/com/example/smitha/parkingassistant/Sign_in.java
+++ app/src/main/java/com/example/smitha/parkingassistant/Sign_in.java
@@ -1,13 +1,123 @@
 package com.example.smitha.parkingassistant;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.app.ProgressDialog;
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Sign_in extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    //private ProgressBar progressBar;
+    private ProgressDialog progressDialog;
+    private Button btnLogin;
+    private TextView btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(Sign_in.this, File_Upload.class));
+            finish();
+        }
+
+        // set the view now
         setContentView(R.layout.activity_sign_in);
+
+
+
+        inputEmail = (EditText) findViewById(R.id.inputEmail);
+        inputPassword = (EditText) findViewById(R.id.inputPassword);
+        progressDialog = new ProgressDialog(this);
+
+
+        btnSignup = (TextView) findViewById(R.id.btnSignup);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Sign_in.this, Signup.class));
+            }
+        });
+
+        /*btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+            }
+        });*/
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressDialog.setMessage("Registering Please Wait...");
+                progressDialog.show();
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Sign_in.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+
+                                if (!task.isSuccessful()) {
+
+                                        Toast.makeText(Sign_in.this,"Succesfull", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(Sign_in.this, File_Upload.class);
+                                    startActivity(intent);
+
+                                }
+                                else {
+                                    Intent intent = new Intent(Sign_in.this, Sign_in.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                progressDialog.dismiss();
+
+                            }
+                        });
+            }
+        });
     }
 }
